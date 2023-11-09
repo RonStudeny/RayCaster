1879f70645e8b037ed0abfc8c1fbcc89265fd822
Ron Studený <ronstudeny@gmail.com>
2023-11-09 08:19:20 +0100

    autoresize the map

--- RayCaster.cpp
+++ RayCaster.cpp
@@ -2,8 +2,8 @@
 
 #define MAP_WIDTH 24
 #define MAP_HEIGHT 24
-#define SCREEN_WIDTH 24
-#define SCREEN_HEIGHT 24
+#define SCREEN_WIDTH 96
+#define SCREEN_HEIGHT 96
 
 #include "olcPixelGameEngine.h"
 
@@ -61,17 +61,31 @@ public:
 		//		}
 		//	}
 		//		//Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
-		for (int x = 0; x < (int)(sizeof(map[0]) / sizeof(int)); x++)
-			for (int y = 0; y < (int)(sizeof(map) / sizeof(map[0])); y++) {
+		int mapWidth = (int)(sizeof(map[0]) / sizeof(int));
+		int mapHeight = (int)(sizeof(map) / sizeof(map[0]));
+
+		float cellWidth = (float)SCREEN_WIDTH / mapWidth;
+		float cellHeight = (float)SCREEN_HEIGHT / mapHeight;
+
+		for (int x = 0; x < mapWidth; x++)
+			for (int y = 0; y < mapHeight; y++) {
+
+				int cellX = x * cellWidth;
+				int cellY = y * cellHeight;
+				int cellSizeX = (x + 1) * cellWidth - cellX;
+				int cellSizeY = (y + 1) * cellHeight - cellY;
+
 				switch (map[(int)(y)][(int)(x)]) {
 				case 0:
-					Draw(x, y, olc::Pixel(0, 0, 0));
+					//Draw(x, y, olc::Pixel(0, 0, 0));
+					FillRect(cellX, cellY, cellSizeX, cellSizeY, olc::Pixel(0, 0, 0));
 					break;
 				case 1:
-					Draw(x, y, olc::Pixel(105, 105, 105));
+					FillRect(cellX, cellY, cellSizeX, cellSizeY, olc::Pixel(105, 105, 105));
 					break;
 				default:
-					Draw(x, y, olc::Pixel(105, 105, 105));
+					FillRect(cellX, cellY, cellSizeX, cellSizeY, olc::Pixel(255, 0, 0));
+					
 				}
 			}
 
